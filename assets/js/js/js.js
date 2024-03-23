d8db01ad9d54a22aeac5eb775e51738232fe50eb
James <james.dereck.foreman@gmail.com>
2024-03-23 01:04:09 -0700

    Version medio terminada

--- assets/js/js/js.js
+++ assets/js/js/js.js
@@ -6,41 +6,67 @@ $(document).ready(function() {
       var password = $('#password').val();
   
       // Verificar las credenciales
-      if (username === 'james' && password === 'gato') {
+      if (username === 'profe_cesar' && password === 'pongameunsiete') {
         // Credenciales válidas, redirigir a la pantalla de wallet
         window.location.href = '/assets/pages/ewallet.html';
       } else {
         // Credenciales inválidas, mostrar mensaje de error y credenciales correctas
-        alert('Usuario o contraseña invalido. Inténtalo de nuevo. (EL USUARIO ES JAMES, LA CLAVE ES GATO)');
+        alert('Usuario o contraseña invalido. Inténtalo de nuevo. (EL USUARIO ES "profe_cesar", LA CLAVE ES "pongameunsiete")');
+      }
+    });
+});
+  //inicio de sesion mutante
+$(document).ready(function() {
+    $('#formulario').submit(function(event) {
+      event.preventDefault();
+      var username = $('#username').val();
+      var password = $('#password').val();
+  
+      // Verificar las credenciales
+      if (username === 'profe_cesar' && password === 'pongameunsiete') {
+        // Credenciales válidas, redirigir a la pantalla de wallet
+        window.location.href = '/assets/pages/ewallet.html';
+      } else {
+        // Credenciales inválidas, mostrar mensaje de error y credenciales correctas
+        alert('Usuario o contraseña invalido. Inténtalo de nuevo. (Usuario: profesor_cesar, clave: pongameunsiete)');
       }
     });
   });
   
-  $(document).ready(function() {
-    // saldo inicial
-    let balance = 100.00;
+  function credenciales() {
+  alert("Usuario: profe_cesar Clave: pongameunsiete");
+}
 
-    // mostrar saldo
-    $('#balance').text(balance.toFixed(1));
+$(document).ready(function () {
+  // saldo inicial
+  let balance = 0;
 
-    // agregar boton de transaccion
-    $('#add-transaction').click(function() {
-        const amount = parseFloat(prompt('Ingresa la cantidad de dinero:'));
-        if (!isNaN(amount)) {
-            // Recargar saldo
-            balance += amount;
-            $('#balance').text(balance.toFixed(1));}
-   
-  // Subtract transaction button click event
-   $('#subtract-transaction').click(function() {
+  // mostrar saldo
+  $('#balance').text(balance.toFixed(1));
+
+  // agregar boton de transaccion
+  $('#add-transaction').click(function () {
     const amount = parseFloat(prompt('Ingresa la cantidad de dinero:'));
     if (!isNaN(amount)) {
+      // Recargar saldo
+      balance += amount;
+      $('#balance').text(balance.toFixed(1));
+    }
+    
+    // Subtract transaction button click event
+    $('#subtract-transaction').click(function () {
+      const amount = parseFloat(prompt('Ingresa la cantidad de dinero:'));
+      if (!isNaN(amount)) {
         // Recargar saldo
         balance -= amount;
         $('#balance').text(balance.toFixed(1));
-
-            // Agregar a la historia
-            $('#transaction-history').append(`<li>${amount > 0 ? '+' : '-'} $${Math.abs(amount.toFixed())}</li>`);
-        }
-    });
-});})
+      }
+    })
+  })
+})
+function boton_buscar() {
+  alert("Buscando Contacto");
+}
+function modal_agregar_contacto() {
+  alert("Contacto agregado con exito");
+}
