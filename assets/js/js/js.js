a804d2401a99d4a67242e330545b3a3a13f4bb1e
James <james.dereck.foreman@gmail.com>
2024-03-24 18:06:35 -0700

    Version finalizada

--- assets/js/js/js.js
+++ assets/js/js/js.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
       // Verificar las credenciales
       if (username === 'profe_cesar' && password === 'pongameunsiete') {
         // Credenciales válidas, redirigir a la pantalla de wallet
-        window.location.href = '/assets/pages/ewallet.html';
+        window.location.href = '/assets/pages/menu.html';
       } else {
         // Credenciales inválidas, mostrar mensaje de error y credenciales correctas
-        alert('Usuario o contraseña invalido. Inténtalo de nuevo. (EL USUARIO ES "profe_cesar", LA CLAVE ES "pongameunsiete")');
+        alert('Usuario o contraseña invalido. Inténtalo de nuevo.');
       }
     });
 });
-  //inicio de sesion mutante
+  //inicio de sesion *arreglo
 $(document).ready(function() {
     $('#formulario').submit(function(event) {
       event.preventDefault();
@@ -25,7 +25,7 @@ $(document).ready(function() {
       // Verificar las credenciales
       if (username === 'profe_cesar' && password === 'pongameunsiete') {
         // Credenciales válidas, redirigir a la pantalla de wallet
-        window.location.href = '/assets/pages/ewallet.html';
+        window.location.href = '/assets/pages/menu.html';
       } else {
         // Credenciales inválidas, mostrar mensaje de error y credenciales correctas
         alert('Usuario o contraseña invalido. Inténtalo de nuevo. (Usuario: profesor_cesar, clave: pongameunsiete)');
@@ -63,10 +63,39 @@ $(document).ready(function () {
       }
     })
   })
-})
+})//alerta contactos
 function boton_buscar() {
   alert("Buscando Contacto");
 }
 function modal_agregar_contacto() {
   alert("Contacto agregado con exito");
 }
+  document.addEventListener("DOMContentLoaded", function() {
+    var welcomeText = document.getElementById("welcomeText");
+    welcomeText.classList.add("fade-in");
+  });
+  // Datos de transaccion
+  var transactions = [
+    { date: "2024-03-20", description: "Pago Recibido", amount: 80 },
+    { date: "2024-03-22", description: "Compra Online", amount: -50 },
+    { date: "2024-03-25", description: "Retiro de Fondos", amount: -30 },
+  ];
+
+  // Funcion para poblar la tabla
+  function populateTransactions() {
+    var transactionTable = document.getElementById("transactionTable");
+    transactions.forEach(function(transaction) {
+      var row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${transaction.date}</td>
+        <td>${transaction.description}</td>
+        <td>${transaction.amount}</td>
+      `;
+      transactionTable.appendChild(row);
+    });
+  }
+
+  // Llamar a la funcion cuando carga la pagina
+  window.onload = function() {
+    populateTransactions();
+  };
